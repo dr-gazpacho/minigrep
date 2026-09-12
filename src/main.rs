91a904ddbe58799c1e63cfe179f3737f31fde0cd
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2026-09-12 16:29:22 -0500

    extract into parse config function

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {query}");
     println!("In file {file_path}");
@@ -14,4 +13,11 @@ fn main() {
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("Contents: {contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    (query, file_path)
 }
