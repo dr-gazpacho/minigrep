613ec7e0975ec05c8c1a6f84d8637ffdb3b46526
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2026-09-12 17:57:07 -0500

    welcoming environment (variables)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 use std::{env, error::Error, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,8 +18,9 @@ fn main() {
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -30,7 +31,9 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
@@ -38,7 +41,13 @@ impl Config {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
